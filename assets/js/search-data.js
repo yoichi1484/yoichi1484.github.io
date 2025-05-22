2455b63602d1696a56a6101c09b73b8d94a2ed0c
yoichi1484 <yoichi1484@gmail.com>
2025-05-22 09:36:42 +0000

    Deploying to gh-pages from @ yoichi1484/yoichi1484.github.io@a3467f7294de71733a9e27e8e4d76a848e9ddbb1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,14 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-have-joined-kyoto-university-as-a-postdoctoral-researcher",
+          title: 'I have joined Kyoto University as a postdoctoral researcher.',
+          description: "",
+          section: "News",},{id: "news-our-paper-evaluating-the-robustness-of-discrete-prompts-has-been-accepted-to-eacl-2023",
+          title: 'Our paper Evaluating the Robustness of Discrete Prompts has been accepted to EACL...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
