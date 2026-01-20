4c3bf555e3134badf09b87d0a85263b89f7d827c
yoichi1484 <yoichi1484@gmail.com>
2026-01-20 01:13:28 +0000

    Deploying to gh-pages from @ yoichi1484/yoichi1484.github.io@2a744b04b3093208e95e422f02b8d8219fac9e5a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/cv.pdf";
           },
         },{id: "nav-repositories",
           title: "Repositories",
